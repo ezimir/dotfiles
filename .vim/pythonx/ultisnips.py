c1060e08eaecf6f0d90a7c3f785e5d4374d614d0
Martin Tóth <ezimir@gmail.com>
2015-07-19 21:38:15 +0200

    Adding context sensitive snippets for __init__ and super()

--- .vim/pythonx/ultisnips.py
+++ .vim/pythonx/ultisnips.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+
+import re
+
 
 def complete(tabstop, options):
     if tabstop:
@@ -7,4 +13,44 @@ def complete(tabstop, options):
         return options[0]
 
     return '({options})'.format(options = '|'.join(options))
+
+
+def within_class(buffer, line):
+    line_number = line - 1
+    current = buffer[line_number]
+    while not re.match(r'\s*class \w.*', current):
+        line_number -= 1
+        if line_number < 0:
+            return False
+
+        current = buffer[line_number]
+
+    else:
+        class_name = re.findall(r'class ([^\(:]+).*:', current)[0]
+
+        return class_name
+
+
+def within_method(buffer, line):
+    class_name = within_class(buffer, line)
+    if not class_name:
+        return False
+
+    line_number = line - 1
+    current = buffer[line_number]
+    while not re.match(r'\s*def \w.*', current):
+        line_number -= 1
+        if re.match(r'\s*class \w.*', current) or line_number < 0:
+            return False
+
+        current = buffer[line_number]
+
+    else:
+        if '(self' not in current:
+            return False
+
+        func_name = re.findall(r'def ([^\(]+)\(', current)[0]
+        func_attrs = re.findall(r'\(self(?:, )?(.*)\)', current)[0]
+
+        return class_name, func_name, func_attrs
 
