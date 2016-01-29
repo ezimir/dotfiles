4eaeba333fa425946cca2a58ed7f9fcc8e09b709
Martin Tóth <ezimir@gmail.com>
2016-01-29 15:52:48 -0500

    Update context variable passing for snippet functions

--- .vim/pythonx/ultisnips.py
+++ .vim/pythonx/ultisnips.py
@@ -18,15 +18,15 @@ def complete(tabstop, options):
     return u'({options})'.format(options = '|'.join(options))
 
 
-def within_class(buffer, line):
-    line_number = line - 1
-    current = buffer[line_number]
+def within_class(snip):
+    line_number = snip.line - 1
+    current = snip.buffer[line_number]
     while not re.match(r'\s*class \w.*', current):
         line_number -= 1
         if line_number < 0:
             return False
 
-        current = buffer[line_number]
+        current = snip.buffer[line_number]
 
     else:
         class_name = re.findall(r'class ([^\(:]+).*:', current)[0]
@@ -34,19 +34,19 @@ def within_class(buffer, line):
         return class_name
 
 
-def within_method(buffer, line):
-    class_name = within_class(buffer, line)
+def within_method(snip):
+    class_name = within_class(snip)
     if not class_name:
         return False
 
-    line_number = line - 1
-    current = buffer[line_number]
+    line_number = snip.line - 1
+    current = snip.buffer[line_number]
     while not re.match(r'\s*def \w.*', current):
         line_number -= 1
         if re.match(r'\s*class \w.*', current) or line_number < 0:
             return False
 
-        current = buffer[line_number]
+        current = snip.buffer[line_number]
 
     else:
         if '(self' not in current:
@@ -56,4 +56,8 @@ def within_method(buffer, line):
         func_attrs = re.findall(r'\(self(?:, )?(.*)\)', current)[0]
 
         return class_name, func_name, func_attrs
+
+
+def within_tag(snip):
+    return re.search('<[^<>]+$', snip.buffer[snip.line - 1][:snip.column])
 
